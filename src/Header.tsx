646e8cd8f750305f7e7381f5f57dce6cbaa60eab
gustavwiese <gusse12345@gmail.com>
2024-05-16 13:55:54 +0200

    added big bowl pic to header

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,7 +1,10 @@
+import bigbowlicon from "./assets/bigbowlicon.png";
+
 export const Header = () => (
   <header className="border-b-[1px] border-border border-solid w-full pb-4 grid grid-cols-2">
     <div>
-      <h1 className="text-2xl font-bold">BigBowl frontend</h1>
+      <img className="max-w-[10rem]" src={bigbowlicon} alt="bigbowlicon" />
+      <h1 className="text-2xl font-bold">BigBowl Frontend</h1>
     </div>
   </header>
 );
