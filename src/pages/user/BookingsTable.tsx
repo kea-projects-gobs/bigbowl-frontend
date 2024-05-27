6c3023ef5f20c02302cd8ca3b9932b58af23cce7
osman-butt <butt.osman@gmail.com>
2024-05-27 13:37:00 +0200

    Added toast

--- src/pages/user/BookingsTable.tsx
+++ src/pages/user/BookingsTable.tsx
@@ -13,31 +13,7 @@ import { DeleteBooking } from "./DeleteBooking";
 import { useEffect, useState } from "react";
 import { deleteReservation, getAllReservations } from "@/services/api/api";
 import { cn } from "@/lib/utils";
-
-// JSON ORDERS
-// {
-//         "id": 1,
-//         "noOfParticipants": 8,
-//         "date": "2024-05-28",
-//         "userName": "Johnersej123",
-//         "reservationItems": [
-//             {
-//                 "id": 1,
-//                 "activityName": "Bowling Lane 1",
-//                 "price": 100.0,
-//                 "startTime": "2024-05-28T10:00:00",
-//                 "endTime": "2024-05-28T11:00:00"
-//             },
-//             {
-//                 "id": 2,
-//                 "activityName": "Bowling Lane 2",
-//                 "price": 100.0,
-//                 "startTime": "2024-05-28T10:00:00",
-//                 "endTime": "2024-05-28T11:00:00"
-//             }
-//         ],
-//         "confirmed": false
-//     }
+import { useToast } from "@/components/ui/use-toast";
 
 type Item = {
   id: number;
@@ -58,6 +34,7 @@ type Order = {
 
 export function BookingsTable() {
   const [orders, setOrders] = useState<Order[]>([]);
+  const { toast } = useToast();
 
   useEffect(() => {
     const fetchOrders = async () => {
@@ -107,6 +84,12 @@ export function BookingsTable() {
     const res = await getAllReservations();
     if (res.status === 200) {
       setOrders(res.data);
+      toast({
+        title: "Reservation slettet",
+        description: "Reservationen er blevet slettet.",
+        variant: "default",
+        className: "bg-green-500 text-white",
+      });
     } else {
       console.log("Failed to fetch orders:", res.status);
     }
